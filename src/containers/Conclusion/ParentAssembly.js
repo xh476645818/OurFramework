bfc297e4287b23832564b461a92fbbd393e884cd
xh476645818 <476645818@qq.com>
2018-09-25 10:57:20 +0800

    提交一些修改，增加了脚手架的新功能，比如echart和lodash，分离打包这类

--- src/containers/Conclusion/ParentAssembly.js
+++ src/containers/Conclusion/ParentAssembly.js
@@ -9,9 +9,9 @@ class Conclusion extends Component {
         super(props, context);
         console.clear();
         console.log('父-周期constructor');
-        this.state={
-            text:'我是一初始的值',
-            data:'我就是一个简单的子组件，你点我一下试试？'
+        this.state = {
+            text: '我是一初始的值',
+            data: '我就是一个简单的子组件，你点我一下试试？'
         }
         this.handle = this.handle.bind(this);
         this.myHandle = this.myHandle.bind(this);
@@ -44,20 +44,23 @@ class Conclusion extends Component {
     componentWillUnmount() {
         console.log('父-周期componentWillUnmount')
     }
+
     handle(e) {
         this.setState({
-            text:'这是一次成功的子传父',
-            data:'我成功的呼叫了我爸爸'
+            text: '这是一次成功的子传父',
+            data: '我成功的呼叫了我爸爸'
         })
         console.log('这是一次成功的子传父');
     }
-    myHandle(){
+
+    myHandle() {
         this.setState({
-            text:'这是一次成功的父传子',
-            data:'你好小崽子们'
+            text: '这是一次成功的父传子',
+            data: '你好小崽子们'
         });
         console.log('这是一次成功的父传子');
     }
+
     render() {
         return (
             <div>

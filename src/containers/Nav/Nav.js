baa43731365258c5628df5f31e7a190780370d08
xh476645818 <476645818@qq.com>
2018-09-28 18:52:17 +0800

    replace

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -14,15 +14,15 @@ export default class App extends React.Component {
                 <p>1了解react的周期，点击链接周期，看组件Conclusion</p>
                 <p>2了解react的父子传值，点击链接AppA，看组件AppA</p>
                 <p>3了解react的redux与thunk，点击链接AppB，看组件AppB</p>
-                <li><NavLink to="/appTest">测试</NavLink></li>
-                <li><NavLink to="/conclusion">周期</NavLink></li>
-                <li><NavLink to="/appA">AppA</NavLink></li>
-                <li><NavLink to="/appB">AppB</NavLink></li>
-                <li><NavLink to="/appC">AppC</NavLink></li>
-                <li><NavLink to="/appD">AppD</NavLink></li>
-                <li><NavLink to="/appLogin">AppLogin</NavLink></li>
-                <li><NavLink to="/appImg">AppImg</NavLink></li>
-                <li><NavLink to="/appRenderCallback">AppRenderCallback</NavLink></li>
+                <li><NavLink to="/appTest" replace>测试</NavLink></li>
+                <li><NavLink to="/conclusion" replace>周期</NavLink></li>
+                <li><NavLink to="/appA" replace>AppA</NavLink></li>
+                <li><NavLink to="/appB" replace>AppB</NavLink></li>
+                <li><NavLink to="/appC" replace>AppC</NavLink></li>
+                <li><NavLink to="/appD" replace>AppD</NavLink></li>
+                <li><NavLink to="/appLogin" replace>AppLogin</NavLink></li>
+                <li><NavLink to="/appImg" replace>AppImg</NavLink></li>
+                <li><NavLink to="/appRenderCallback" replace>AppRenderCallback</NavLink></li>
             </div>
         )
     }

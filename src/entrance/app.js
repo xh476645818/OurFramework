d1e212b78fd9f0e7d520e87a8d93ff800ad14f59
xh476645818 <476645818@qq.com>
2018-10-08 11:07:16 +0800

    加入蚂蚁金服的声明

--- src/entrance/app.js
+++ src/entrance/app.js
@@ -1,14 +1,23 @@
 /**
  * Created by xiaohe on 2018/5/7.
  */
+//mock加载
+import 'mock/index';
+//路由
 import Router from 'con/Router/Router.js';
+//react-redux的组件
 import {Provider} from 'react-redux'
+//所有挂在store
 import store from 'store/index.js';
+//蚂蚁金服国际化
+import {LocaleProvider} from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 
-import Mock from 'mock/index';
 ReactDOM.render(
     <Provider store={store}>
-        <Router/>
+        <LocaleProvider locale={zhCN}>
+            <Router/>
+        </LocaleProvider>
     </Provider>
     , document.getElementById('main')
 )
